b6d47f6c64c2356fbdaf64f59af492ffc3a2c797
M-khawar <>
2022-12-02 09:19:37 +0500

    fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -424,10 +424,6 @@
                             <dl class="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
                                 <dt class="sr-only">Publish at:</dt>
                                 <dd class="flex items-center">
-                                    <svg width="2" height="2" aria-hidden="true" fill="currentColor"
-                                         class="mx-3 text-slate-300">
-                                        <circle cx="1" cy="1" r="1"/>
-                                    </svg>
                                     <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2"
                                          stroke-linecap="round" stroke-linejoin="round"
                                          class="mr-1 text-slate-400 dark:text-slate-500" aria-hidden="true">
@@ -435,7 +431,7 @@
                                             d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z"/>
                                         <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
                                     </svg>
-                                    {{ $feed->pub_date }}
+                                    <small>{{ $feed->pub_date }}</small>
                                 </dd>
                             </dl>
                             <div>
